9cd3ad1d5a588abc4dafde67003470ff25ecce8c
Md Aurangzeb <maduarngjeebmm_cse17@its.edu.in>
2024-01-12 23:48:42 +0530

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,27 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
+import './Login.scss'
 
 
-const Login = ({setUser}) => {
+const Login = ({ setUser }) => {
   return (
     <div className='login__div'>
-        <h3>Linkedin login page</h3>
-<GoogleLogin
-  onSuccess={credentialResponse => {
-    let user = jwtDecode(credentialResponse.credential)
-    localStorage.setItem('profile',user)
-  setUser(true)
-    console.log(user)
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>;
+      <h3>Linkedin login page</h3>
+      {/* <img src="" alt="" /> */}
+      <div className="login__btn">
+
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            let user = jwtDecode(credentialResponse.credential)
+            localStorage.setItem('profile', user)
+            setUser(true)
+            console.log(user)
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />;
+      </div>
     </div>
   )
 }
